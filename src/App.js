995fb00601efc547858c7708c038f644ff299133
savisnapuu <savisnapuu@gmail.com>
2022-07-11 21:50:05 +0300

    m

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 import Greet from './components/Greet';
+import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
+      <Counter />
+      <Message />
       <Greet name="Bruce" heroName="Batman">
         <p>This is Children paragraph</p>
       </Greet>
@@ -13,7 +17,7 @@ function App() {
         <button>Action</button>
       </Greet>
       <Greet name="Diana" heroName="Wonderwoman">
-        <buton>Action2</buton>
+        <button>Action2</button>
       </Greet>
     </div>
   );
